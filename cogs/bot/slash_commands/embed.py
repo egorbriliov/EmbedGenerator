6a8ebb2470e99289f4a106e688dce32aa9d3d4f2
Егор Брилёв <egor.briliov@gmai.com>
2024-07-22 12:39:41 +0300

    List I'm what done: 1. Create checkers for url inputs 2. Create works handlers for buttons and modals 4. Create required_permissions for /embed

--- cogs/bot/slash_commands/embed.py
+++ cogs/bot/slash_commands/embed.py
@@ -2,6 +2,7 @@ import datetime
 import validators
 
 import disnake
+from aiogram.client.session import aiohttp
 from disnake.ext import commands
 
 from PIL import Image
@@ -53,13 +54,30 @@ class ModalGenerator(disnake.ui.Modal):
         self.bot = bot
         self.setting_name = setting_name
         self.embed_dict = embed_dict
-        if setting_name in ["description", "title", "thumbnail", "image"]:
+        if setting_name in ["description", "title"]:
+            if setting_name == "description":
+                style = disnake.TextInputStyle.long
+            else:
+                style = disnake.TextInputStyle.short
             components = [
                 disnake.ui.TextInput(label=self.setting_name.title(),
                                      placeholder=f"Enter embed {self.setting_name}",
                                      custom_id=setting_name,
-                                     required=True)
+                                     required=True,
+                                     style=style)
             ]
+        elif setting_name == "image":
+            components = [
+                disnake.ui.TextInput(label="Image url",
+                                     placeholder=f"Enter image url",
+                                     custom_id="url",
+                                     required=True)]
+        elif setting_name == "thumbnail":
+            components = [
+                disnake.ui.TextInput(label="Thumbnail url",
+                                     placeholder=f"Enter thumbnail url",
+                                     custom_id="url",
+                                     required=True)]
         elif setting_name == "color":
             components = [
                 disnake.ui.TextInput(label="Color Name",
@@ -87,8 +105,8 @@ class ModalGenerator(disnake.ui.Modal):
                                      placeholder=f"Enter footer text",
                                      custom_id="text",
                                      required=True),
-                disnake.ui.TextInput(label="Author url",
-                                     placeholder=f"Enter author url",
+                disnake.ui.TextInput(label="Icon url",
+                                     placeholder=f"Enter icon url",
                                      custom_id="icon_url",
                                      required=False)
             ]
@@ -97,11 +115,12 @@ class ModalGenerator(disnake.ui.Modal):
                 disnake.ui.TextInput(label="Field name",
                                      placeholder=f"Enter field name",
                                      custom_id="name",
-                                     required=False),
+                                     required=True),
                 disnake.ui.TextInput(label="Field value",
                                      placeholder=f"Enter field value",
                                      custom_id="value",
-                                     required=False),
+                                     required=False,
+                                     style=disnake.TextInputStyle.long),
                 disnake.ui.TextInput(label="Field inline?",
                                      placeholder=f"True or False",
                                      custom_id="inline",
@@ -139,7 +158,6 @@ class ModalGenerator(disnake.ui.Modal):
             setting_name = interaction.text_values[self.setting_name]
             self.embed_dict[self.setting_name] = setting_name
             await add_good()
-
         elif self.setting_name == "color":
             def is_valid_hex_str(hex_str):
                 """
@@ -160,12 +178,10 @@ class ModalGenerator(disnake.ui.Modal):
                 await interaction.send("Not correct color HEX. Hex format must be 0x123abc!",
                                        delete_after=5,
                                        ephemeral=True)
-
         elif self.setting_name == "footer":
             if interaction.text_values["icon_url"]:
                 if is_valid_image_url(interaction.text_values["icon_url"]):
                     self.embed_dict[self.setting_name] = interaction.text_values
-                    print(self.embed_dict[self.setting_name])
                     await add_good()
                 else:
                     await interaction.send(f"You need past correct icon url!", delete_after=5,
@@ -174,42 +190,83 @@ class ModalGenerator(disnake.ui.Modal):
                 self.embed_dict[self.setting_name] = interaction.text_values
                 await add_good()
         elif self.setting_name == "author":
-            if interaction.text_values["icon_url"] or interaction.text_values["url"]:
+            if interaction.text_values["icon_url"] and interaction.text_values["url"]:
                 if is_valid_image_url(interaction.text_values["icon_url"]) and is_valid_url(
                         interaction.text_values["url"]):
                     self.embed_dict[self.setting_name] = interaction.text_values
                     await add_good()
-
-                if not is_valid_image_url(interaction.text_values["icon_url"]):
+                elif is_valid_image_url(interaction.text_values["icon_url"]) and not is_valid_url(
+                        interaction.text_values["url"]):
+                    await interaction.send(f"You need past correct author url!", delete_after=5,
+                                           ephemeral=True)
+                elif not is_valid_image_url(interaction.text_values["icon_url"]) and is_valid_url(
+                        interaction.text_values["url"]):
                     await interaction.send(f"You need past correct icon url!", delete_after=5,
                                            ephemeral=True)
-                if not is_valid_url(interaction.text_values["url"]):
-                    await interaction.send(f"You need past correct url!", delete_after=5,
+                elif not is_valid_image_url(interaction.text_values["icon_url"]) and not is_valid_url(
+                        interaction.text_values["url"]):
+                    await interaction.send(f"You need past correct author url and icon url!", delete_after=5,
                                            ephemeral=True)
-            else:
-                self.embed_dict[self.setting_name] = interaction.text_values
-                await add_good()
+
+            elif interaction.text_values["icon_url"] and not interaction.text_values["url"]:
+                if is_valid_image_url(interaction.text_values["icon_url"]):
+                    self.embed_dict[self.setting_name] = interaction.text_values
+                    await add_good()
+                elif not is_valid_image_url(interaction.text_values["icon_url"]):
+                    await interaction.send(f"You need past correct icon url!", delete_after=5,
+                                           ephemeral=True)
+            elif not interaction.text_values["icon_url"] and interaction.text_values["url"]:
+                if is_valid_image_url(interaction.text_values["url"]):
+                    self.embed_dict[self.setting_name] = interaction.text_values
+                    await add_good()
+                elif not is_valid_image_url(interaction.text_values["url"]):
+                    await interaction.send(f"You need past correct author url!", delete_after=5,
+                                           ephemeral=True)
         elif self.setting_name in ["thumbnail", "image"]:
-            self.embed_dict[self.setting_name] = {"url": interaction.text_values[self.setting_name]}
-            await interaction.response.edit_message(embed=disnake.Embed.from_dict(self.embed_dict),
-                                                    view=MyView(self.embed_dict, bot=self.bot))
-        elif self.setting_name == "fields":
-            if self.embed_dict.get("fields"):
-                self.embed_dict["fields"].append(interaction.text_values)
-            else:
-                self.embed_dict["fields"] = [interaction.text_values]
-            try:
+            if interaction.text_values["url"] and is_valid_image_url(interaction.text_values["url"]):
+                self.embed_dict[self.setting_name] = {"url": interaction.text_values["url"]}
                 await interaction.response.edit_message(embed=disnake.Embed.from_dict(self.embed_dict),
                                                         view=MyView(self.embed_dict, bot=self.bot))
-            except Exception as E:
-                print(E)
-                print(f"embed_dict: \n{self.embed_dict}")
+            else:
+                await interaction.send(f"You need past correct url!", delete_after=5,
+                                       ephemeral=True)
+        elif self.setting_name == "fields":
+            data = interaction.text_values
+            try:
+                fields_name_list = [field.get("name") for field in self.embed_dict["fields"]]
+            except:
+                fields_name_list = []
+            if data["name"] not in fields_name_list:
+                if data["value"] or data["name"]:
+                    if not data["inline"]:
+                        data["inline"] = False
+                    if self.embed_dict.get("fields"):
+                        self.embed_dict["fields"].append(data)
+                    else:
+                        self.embed_dict["fields"] = [data]
+                    try:
+                        await interaction.response.edit_message(embed=disnake.Embed.from_dict(self.embed_dict),
+                                                                view=MyView(self.embed_dict, bot=self.bot))
+                    except Exception:
+                        pass
+                else:
+                    await interaction.send(f"You need to pass value or name!", delete_after=5,
+                                           ephemeral=True)
+            else:
+                await interaction.send(f"This name is already in use!", delete_after=5, ephemeral=True)
         elif self.setting_name == "webhook":
-            webhook_url = interaction.text_values[self.setting_name]
-            # Создаем объект вебхука
-            webhook = disnake.Webhook.from_url(url=webhook_url, session=self.bot)
-            # Отправляем сообщение через вебхук
-            await webhook.send(content="Привет, это сообщение отправлено через вебхук!")
+            try:
+                webhook_url = interaction.text_values[self.setting_name]
+                async with aiohttp.ClientSession() as webhook:
+                    webhook = disnake.Webhook.from_url(url=webhook_url, session=webhook)
+                    await webhook.send(embed=disnake.Embed.from_dict(self.embed_dict))
+                    await interaction.response.edit_message(f"Embed was sent", components=None, embed=None)
+            except Exception:
+                await interaction.response.edit_message("How do you want to send embed?",
+                                                        view=SendSelectMenu(self.embed_dict, bot=self.bot),
+                                                        embed=None)
+                await interaction.send(f"You need past correct url!", delete_after=5,
+                                       ephemeral=True)
 
 
 class ButtonGenerator(disnake.ui.Button):
@@ -250,7 +307,7 @@ class ButtonGenerator(disnake.ui.Button):
 
         elif setting_name == "fields":
             if self.embed_dict.get("fields"):
-                if len(self.embed_dict.get("fields")) < 3:
+                if len(self.embed_dict.get("fields")) < 15:
                     super().__init__(
                         label=setting_name.title(),
                         style=disnake.ButtonStyle.green,
@@ -306,30 +363,24 @@ class ButtonGenerator(disnake.ui.Button):
                                                                      setting_name=self.setting_name, bot=self.bot))
             # Не могут добавляться и без description or title
             elif self.setting_name in ["timestamp", "color"]:
-                if "description" in self.embed_dict or "title" in self.embed_dict:
-                    if self.setting_name == "timestamp":
-                        self.embed_dict[self.setting_name] = datetime.datetime.now().isoformat()
-                        await interaction.response.edit_message(f"{self.setting_name.title()} has been added",
-                                                                embed=disnake.Embed.from_dict(self.embed_dict),
-                                                                view=MyView(self.embed_dict, bot=self.bot))
-                    if self.setting_name == "color":
-                        await interaction.response.send_modal(ModalGenerator(embed_dict=self.embed_dict,
-                                                                             setting_name=self.setting_name,
-                                                                             bot=self.bot))
-                else:
-                    await interaction.response.send_message(f"At the first you need to add something else!",
-                                                            delete_after=5, ephemeral=True)
+                if self.setting_name == "timestamp":
+                    self.embed_dict[self.setting_name] = datetime.datetime.now().isoformat()
+                    await interaction.response.edit_message(embed=disnake.Embed.from_dict(self.embed_dict),
+                                                            view=MyView(self.embed_dict, bot=self.bot))
+                if self.setting_name == "color":
+                    await interaction.response.send_modal(ModalGenerator(embed_dict=self.embed_dict,
+                                                                         setting_name=self.setting_name,
+                                                                         bot=self.bot))
             # Для всех прочих (fields): удаляет из данных и отсылает ответ
             elif self.setting_name:
                 if "fields" in self.embed_dict:
                     if self.setting_name in [field.get("name") for field in self.embed_dict["fields"]]:
-                        # Действие
                         fields = self.embed_dict["fields"]
                         if len(fields) > 1:
                             for field in fields:
                                 if self.setting_name == field.get("name"):
                                     fields.remove(field)
-                                    return
+                                    self.embed_dict.update({"fields": fields})
                         elif len(fields) == 1:
                             del self.embed_dict["fields"]
 
@@ -402,22 +453,40 @@ class EmbedGenerator(commands.Cog):
         """
         self.bot = bot
 
-    @commands.slash_command(name="embed")
-    async def category(self,
-                       inter: disnake.ApplicationCommandInteraction
-                       ):
-        """
-        Срабатывает на: запуск.
-        -----------------------------
-        Отвечает за: формирование правил.
-        """
+    @commands.slash_command(name="embed", description="Embed creator")
+    async def embed(self,
+                    inter: disnake.ApplicationCommandInteraction
+                    ):
         if inter.guild:
-            if inter.user.id == inter.guild.owner_id:
+            required_permissions = [
+                "view_channel",
+                "send_messages",
+                "embed_links",
+                "attach_files"
+            ]
+
+            user_channel_permissions = inter.channel.permissions_for(inter.user)
+            member_missing_permissions: list = [perm
+                                                for perm in required_permissions
+                                                if not getattr(user_channel_permissions, perm)]
+
+            bot_member = inter.guild.get_member(inter.bot.user.id)
+            bot_channel_permissions = inter.channel.permissions_for(bot_member)
+            bot_missing_permissions: list = [perm
+                                             for perm in required_permissions
+                                             if not getattr(bot_channel_permissions, perm)]
+
+            if not member_missing_permissions and not bot_missing_permissions:
                 await inter.response.send_message(view=MyView(embed_dict={}, bot=self.bot), ephemeral=True)
-            else:
-                await inter.response.send_message("You are not server Administrator",
-                                                  ephemeral=True,
-                                                  delete_after=10)
+            elif member_missing_permissions:
+                await inter.response.send_message(f"**You missing permissions:** \n• "
+                                                  f"{'\n• '.join(map(str, member_missing_permissions))}",
+                                                  ephemeral=True, delete_after=10)
+            elif bot_missing_permissions:
+                print(bot_missing_permissions)
+                await inter.response.send_message(f"**<@1261557247113166859> missing permissions:** \n• "
+                                                  f"{'\n• '.join(map(str, bot_missing_permissions))}",
+                                                  ephemeral=True, delete_after=5)
         else:
             await inter.response.send_message(view=MyView(embed_dict={}, bot=self.bot), ephemeral=True)
 
